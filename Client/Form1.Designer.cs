ebb2da2b3653bed970a478573294d9ac6cac750d
azvm <azvm@protonmail.com>
2019-09-19 08:35:51 +0300

    Add client

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.numericUpDownX = new System.Windows.Forms.NumericUpDown();
+            this.labelPlus = new System.Windows.Forms.Label();
+            this.numericUpDownY = new System.Windows.Forms.NumericUpDown();
+            this.buttonEquals = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownX
+            // 
+            this.numericUpDownX.Location = new System.Drawing.Point(12, 12);
+            this.numericUpDownX.Name = "numericUpDownX";
+            this.numericUpDownX.Size = new System.Drawing.Size(169, 20);
+            this.numericUpDownX.TabIndex = 0;
+            // 
+            // labelPlus
+            // 
+            this.labelPlus.AutoSize = true;
+            this.labelPlus.Location = new System.Drawing.Point(90, 51);
+            this.labelPlus.Name = "labelPlus";
+            this.labelPlus.Size = new System.Drawing.Size(13, 13);
+            this.labelPlus.TabIndex = 1;
+            this.labelPlus.Text = "+";
+            // 
+            // numericUpDownY
+            // 
+            this.numericUpDownY.Location = new System.Drawing.Point(12, 83);
+            this.numericUpDownY.Name = "numericUpDownY";
+            this.numericUpDownY.Size = new System.Drawing.Size(169, 20);
+            this.numericUpDownY.TabIndex = 2;
+            // 
+            // buttonEquals
+            // 
+            this.buttonEquals.Location = new System.Drawing.Point(12, 122);
+            this.buttonEquals.Name = "buttonEquals";
+            this.buttonEquals.Size = new System.Drawing.Size(169, 23);
+            this.buttonEquals.TabIndex = 3;
+            this.buttonEquals.Text = "=";
+            this.buttonEquals.UseVisualStyleBackColor = true;
+            this.buttonEquals.Click += new System.EventHandler(this.ButtonEquals_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(12, 164);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(13, 13);
+            this.labelResult.TabIndex = 4;
+            this.labelResult.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(193, 195);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonEquals);
+            this.Controls.Add(this.numericUpDownY);
+            this.Controls.Add(this.labelPlus);
+            this.Controls.Add(this.numericUpDownX);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownX;
+        private System.Windows.Forms.Label labelPlus;
+        private System.Windows.Forms.NumericUpDown numericUpDownY;
+        private System.Windows.Forms.Button buttonEquals;
+        private System.Windows.Forms.Label labelResult;
     }
 }
 
--- Client/Form1.cs
+++ Client/Form1.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +8,44 @@ namespace Client
 {
     public partial class Form1 : Form
     {
+        private static readonly HttpClient client = new HttpClient();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async Task SendRequest()
+        {
+            try
+            {
+                var values = new Dictionary<string, string>
+                {
+                    ["x"] = numericUpDownX.Value.ToString(),
+                    ["y"] = numericUpDownY.Value.ToString()
+                };
+
+                using (var content = new FormUrlEncodedContent(values))
+                {
+
+                    HttpResponseMessage response;
+
+                    response = await client.PostAsync("http://localhost/LabWork3/xxx.sum", content);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    labelResult.Text = responseString;
+                }
+            }
+            catch (Exception error)
+            {
+                labelResult.Text = error.Message;
+            }
+        }
+
+        private async void ButtonEquals_Click(object sender, EventArgs e)
+        {
+            await SendRequest();
         }
     }
 }
